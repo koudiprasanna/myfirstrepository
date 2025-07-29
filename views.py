2624bd69333a021874d3418d071bf6badd0ce6ee
prasanna <koudiprasanna1905@gmail.com>
2025-07-29 12:43:12 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+# def homepage(request):
+#     return HttpResponse("THIS IS TEXT IS FROM HOMEPAGE")
+
+# from django.http import HttpResponse
+
+# from django.http import HttpResponse
+
+# def contact(request):
+#     return HttpResponse("This is the contact page.")
+from django .http import HttpResponse
+def home(request):
+    return HttpResponse("welcome to the home page!")
